fc3331a641be8bb5ab46d894267e168e92e73530
szajberpiraty <szajberpiraty@gmail.com>
2022-01-26 11:30:13 +0100

    stringek, karakterek

---
+++ Valtozok/Stringek/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string szoveg = "Valami szöveg";
+            Console.WriteLine($"A szöveg hossza:{szoveg.Length}");
+            
+            //Nagybetűsre alakítás
+            Console.WriteLine(szoveg.ToUpper());
+            //Kisbetűsre alakítás
+            Console.WriteLine(szoveg.ToLower());
+
+            //Kezdődik-e valamilyen karakterrel, vagy stringgel
+            Console.WriteLine(szoveg.StartsWith("Valaki"));
+            //Valamilyen karakterre/stringre végződik-e?
+            Console.WriteLine(szoveg.EndsWith("x"));
+            //Tartalmaz-e egy stringet a változó?
+            Console.WriteLine(szoveg.Contains("ami"));
+                         // 0123456789    
+            string datum = "2022.01.26";
+            string ev = datum.Substring(0, 4);
+            string honap = datum.Substring(5, 2);
+            string nap = datum.Substring(8,2);
+
+            Console.WriteLine($"Ev:{ev}");
+            Console.WriteLine($"Honap:{honap}");
+            Console.WriteLine($"Nap:{nap}");
+
+            string[] elemek=datum.Split('.');
+            Console.WriteLine(elemek[0]);
+            Console.WriteLine(elemek[1]);
+            Console.WriteLine(elemek[2]);
+
+            Console.WriteLine(datum.Replace('.', '-'));
+            Console.WriteLine(datum.Replace("01","21"));
+
+            Console.WriteLine(szoveg.Replace(" ",""));
+
+            string szokozok = "-- -@@@  -- szöveg     @--    @--";
+            char[] nemkivanatos = { '-', ' ', '@' };
+            Console.WriteLine(szokozok.Trim(nemkivanatos));
+
+            //Műveletek karakterekkel
+            char c1 = '8';
+            char c2 = '9';
+            char c3 = 'h';
+            
+            Console.WriteLine(Char.IsDigit(c1));
+            Console.WriteLine(Char.IsDigit(c3));
+            Console.WriteLine(Char.IsWhiteSpace(c1));
+
+            //Típus kényszerítése, kasztolás
+            //int osszeg = (int)(Char.GetNumericValue(c1) + Char.GetNumericValue(c2));
+
+            //A fordító rendeli a var esetén a változóhoz a megfelelő típust
+            var osszeg = (Char.GetNumericValue(c1) + Char.GetNumericValue(c2));
+
+            Console.WriteLine($"Összeg:{osszeg}");
+
+            //stringek összehasonlítása
+            string h1 = "ValAmi";
+            string h2 = "vaLaMi";
+
+            if (h1.ToLower()==h2.ToLower())
+            {
+                Console.WriteLine("Megegyeznek");
+            } else
+            {
+                Console.WriteLine("Nem egyeznek meg");
+            }
+
+            string nev1 = "Nagy  eLek";
+            string nev2 = "Nagy Elek";
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- Valtozok/Valtozok/Program.cs
+++ Valtozok/Valtozok/Program.cs
@@ -30,6 +30,54 @@ namespace Valtozok
             Console.WriteLine(Int16.MinValue);
             Console.WriteLine(UInt16.MaxValue);
             Console.WriteLine(UInt16.MinValue);
+            Console.WriteLine(Int64.MaxValue);
+            Console.WriteLine(Int64.MinValue);
+            Console.WriteLine(UInt64.MaxValue);
+            Console.WriteLine(UInt64.MinValue);
+
+            //Lebegőpontos számok
+            float l1 = 122.12345678901234567890f;
+            double l2 = 122.12345678901234567890;
+            decimal l3 = 122.1234567890123456789012345678901234567890m;
+
+            //Karakter - egyetlen betű,szám,írásjel, vezérlő karakter
+            char betu = 'h';
+
+            //Logikai típus igaz/hamis
+            bool logikai = true;
+            logikai = false;
+
+            //szöveg -karakterlánc
+            string szoveg = "Szöveges típus";
+            //a string típus ún. immutable azaz nem változtatható típus
+            szoveg = "Másik szöveg";
+
+            
+
+            Console.WriteLine($"Float:{l1}");
+            Console.WriteLine($"Double:{l2}");
+            Console.WriteLine($"Decimal:{l3}");
+
+            //Aritmetikai műveletek
+
+            //+,-,*,/,%
+
+            //Logikai műveletek 
+            // and,or,not -> &&,||,!
+
+            //relációs műveletek >,<,>=,<=,!=
+
+            //Konvertálás típusok között
+            Console.Write("Adjon meg egy szöveget:");
+            string beolvas = Console.ReadLine();
+            Console.WriteLine(beolvas);
+
+            Console.WriteLine("Adjon meg egy számot:");
+            double szambeolvas = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(szambeolvas);
+
+
+
 
             Console.ReadKey();
         }
