eea8070a21e4bcdc9e86551510a7a03715852b41
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-02-10 13:50:56 +0100

    Elemi algoritmusok

---
+++ ElemiAlgoritmusok/ElemiAlgoritmusok/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElemiAlgoritmusok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] szamok = {2,67,988,345,299,416,629 };
+
+            //összegzés, átlag,megszámlálás
+            int osszeg = 0;
+            int darab = 0;
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                osszeg += szamok[i];
+
+                if (szamok[i]%2==0)
+                {
+                    darab++;
+                }
+                
+            }
+            Console.WriteLine($@"Összeg:{osszeg},
+            Átlag:{osszeg/szamok.Length},
+            Párosok darabszáma:{darab}");
+
+
+            //Min,max meghatározása
+            int min = szamok[0];
+            int max = szamok[0];
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                if (szamok[i]<min)
+                {
+                    min = szamok[i];
+                }
+                if (szamok[i] > max)
+                {
+                    max = szamok[i];
+                }
+                
+            }
+
+            
+
+            Console.WriteLine($"Min:{min},Max:{max}");
+
+            Console.ReadKey();
+        }
+    }
+}
