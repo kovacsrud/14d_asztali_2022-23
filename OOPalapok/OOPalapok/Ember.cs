d5d024133237d16f57430fd09a900b416ed1106e
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-02-15 12:08:31 +0100

    Osztályok

---
+++ OOPalapok/OOPalapok/Ember.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPalapok
+{
+    public class Ember
+    {
+        private string vezeteknev;
+        private string keresztnev;
+        private int szuletesiEv;
+        private string lakhely;
+
+        public int GetEletkor()
+        {
+            return 2022 - szuletesiEv;
+        }
+
+        public void SetVezetekNev(string vezeteknev)
+        {
+            this.vezeteknev = vezeteknev;
+        }
+
+        public void SetKeresztnev(string keresztnev)
+        {
+            this.keresztnev = keresztnev;
+        }
+    }
+}
---
+++ OOPalapok/OOPalapok/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPalapok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4 különböző ember adatait kell tárolni
+            //Vezetéknév,keresztnév,születési év,lakcim
+            //Kérésre az adott ember "árulja el" hogy hány éves
+            //objektum: adatok és metódusok egysége
+            //osztály: az objektum a "tervrajza"
+            //példány: az osztály alapján létrehozott objektum
+
+            //Példányosítás
+            Ember laszlo = new Ember();
+            Ember eva = new Ember();
+
+            
+            laszlo.keresztnev = "László";
+            laszlo.vezeteknev = "Horváth";
+            laszlo.szuletesiEv = 1992;
+            laszlo.lakhely = "Orosháza";
+
+            eva.keresztnev = "Éva";
+            eva.vezeteknev = "Kiss";
+            eva.szuletesiEv = 1986;
+
+            Console.WriteLine(laszlo.vezeteknev+" "+laszlo.keresztnev);
+            Console.WriteLine(laszlo.GetEletkor());
+
+            Console.WriteLine(eva.vezeteknev + " " + eva.keresztnev);
+            Console.WriteLine(eva.GetEletkor());
+
+            Console.ReadKey();
+        }
+    }
+}
