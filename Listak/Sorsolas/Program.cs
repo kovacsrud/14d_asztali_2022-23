d177d66790bb30b27bbc142be3e8a66020e64e55
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-02-15 10:12:33 +0100

    Sorsolás

---
+++ Listak/Sorsolas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorsolas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nyeroSzamok = new List<int>();
+            List<int> sorsoloGomb = new List<int>();
+
+            //Töltse fel a sorsoloGomb-öt 1-90 értékekkel!
+            //válasszon ki belőlük random 5-öt
+            //A kiválasztott szám az a sorsoloGomb-ből
+            //kerüljön át a nyeroSzamok listába
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Listak/VeletlenListak/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeletlenListak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> egeszSzamok = new List<int>();
+            Random rand = new Random();
+
+            var hanySzam = rand.Next(40, 81);
+
+            for (int i = 0; i < hanySzam; i++)
+            {
+                egeszSzamok.Add(rand.Next(-100, 101));
+            }
+
+            Lista(egeszSzamok);
+
+            Console.WriteLine($"Összeg:{egeszSzamok.Sum()}");
+            Console.WriteLine($"Átlag:{egeszSzamok.Average():0.00}");
+
+            //Gyűjtsük ki egy másik listába a negatív számokat!
+
+            var negativak = egeszSzamok.FindAll(x => x < 0 );
+            Lista(negativak);
+
+            
+            var minuszOtven = negativak.Find(x=>x==-50);
+
+            if (egeszSzamok.Any(x=>x>=5 && x<=10))
+            {
+                Console.WriteLine("Tartalmaz!");
+            } else
+            {
+                Console.WriteLine("Nem tartalmaz!");
+            }
+
+
+
+            Console.ReadKey();
+        }
+
+        private static void Lista(List<int> egeszSzamok)
+        {
+            foreach (var i in egeszSzamok)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
