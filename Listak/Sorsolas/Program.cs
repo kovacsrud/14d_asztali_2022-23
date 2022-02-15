1d770c1fd694031cf100b69773152ebaf3cc7544
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-02-15 11:06:33 +0100

    Sorsolás

--- Listak/Sorsolas/Program.cs
+++ Listak/Sorsolas/Program.cs
@@ -12,14 +12,49 @@ namespace Sorsolas
         {
             List<int> nyeroSzamok = new List<int>();
             List<int> sorsoloGomb = new List<int>();
+            Random rand = new Random();
 
             //Töltse fel a sorsoloGomb-öt 1-90 értékekkel!
             //válasszon ki belőlük random 5-öt
             //A kiválasztott szám az a sorsoloGomb-ből
             //kerüljön át a nyeroSzamok listába
+            //Feltöltés
+            for (int i = 1; i < 91; i++)
+            {
+                sorsoloGomb.Add(i);
+            }
+
+            //Lista(sorsoloGomb);
+
+            for (int i = 0; i < 5; i++)
+            {
+                //Ki kell választani egyet véletlenszerűen a
+                //gömbből
+                var kivalasztott = sorsoloGomb[rand.Next(0,sorsoloGomb.Count-1)];
+                nyeroSzamok.Add(kivalasztott);
+                sorsoloGomb.Remove(kivalasztott);
+            }
+
+            Lista(sorsoloGomb);
+            Lista(nyeroSzamok);
+
+            sorsoloGomb.AddRange(nyeroSzamok);
+            nyeroSzamok.Clear();
+
+            Lista(sorsoloGomb);
+            Lista(nyeroSzamok);
 
 
             Console.ReadKey();
+        }
+
+        private static void Lista(List<int> sorsoloGomb)
+        {
+            foreach (var i in sorsoloGomb)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
         }
     }
 }
