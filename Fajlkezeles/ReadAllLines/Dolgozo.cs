8e2c2fac42a50e50d2c83f263a7440e020555518
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-02-23 09:25:12 +0100

    Adatok feldolgozása

---
+++ Fajlkezeles/ReadAllLines/Dolgozo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadAllLines
+{
+    public class Dolgozo
+    {
+        
+
+        public string Nev { get; set; }
+        public string Neme { get; set; }
+        public string Reszleg { get; set; }
+        public int Belepes { get; set; }
+        public int Ber { get; set; }
+
+        public Dolgozo(string sor)
+        {
+            var e = sor.Split(';');
+            Nev = e[0];
+            Neme = e[1];
+            Reszleg = e[2];
+            Belepes = Convert.ToInt32(e[3]);
+            Ber = Convert.ToInt32(e[4]);
+
+        }
+
+
+    }
+}
---
+++ Fajlkezeles/ReadAllLines/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadAllLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dolgozo> dolgozok = new List<Dolgozo>();
+            try
+            {
+                var sorok = File.ReadAllLines("berek2020.txt",Encoding.UTF8);
+                                
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    dolgozok.Add(new Dolgozo(sorok[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);                
+            }
+
+            Console.WriteLine($"Elemek száma:{dolgozok.Count}");
+
+            Console.ReadKey();
+        }
+    }
+}
