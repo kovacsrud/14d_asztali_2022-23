1e11a8f0b9e5401a3494997cfceb4424f495d0e1
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-02-23 11:26:31 +0100

    Update Program.cs

--- Fajlkezeles/ReadAllLines/Program.cs
+++ Fajlkezeles/ReadAllLines/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,11 +12,12 @@ namespace ReadAllLines
     {
         static void Main(string[] args)
         {
+            
             List<Dolgozo> dolgozok = new List<Dolgozo>();
             try
             {
-                var sorok = File.ReadAllLines("berek2020.txt",Encoding.UTF8);
-                                
+                var sorok = File.ReadAllLines("berek2020.txt", Encoding.UTF8);
+
                 for (int i = 1; i < sorok.Length; i++)
                 {
                     dolgozok.Add(new Dolgozo(sorok[i]));
@@ -23,12 +25,74 @@ namespace ReadAllLines
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.StackTrace);                
+                Console.WriteLine(ex.StackTrace);
             }
 
             Console.WriteLine($"Elemek száma:{dolgozok.Count}");
 
+            var nok = dolgozok.FindAll(x => x.Neme == "nő");
+
+            var nokAvgBer = nok.Average(x=>x.Ber);
+
+            Console.WriteLine($"A nők átlagfizetése:{nokAvgBer:#0,0.00} Ft");
+
+            var ferfiak2000 = dolgozok.FindAll(x => x.Neme == "férfi" && x.Belepes >= 2000 && x.Belepes <= 2003);
+            
+            Lista(ferfiak2000);
+
+            //A legjobban kereső nő adatai?
+            var legjobbanKeresoNo = nok.Find(x=>x.Ber==nok.Max(y=>y.Ber));
+            Console.WriteLine($@"A legjobban kereső nő:
+              Név:{legjobbanKeresoNo.Nev}
+              Részleg:{legjobbanKeresoNo.Reszleg}
+              Bér:{legjobbanKeresoNo.Ber}");
+
+            //Hány ember dolgozik az egyes részlegeken? (összesítés feladat)
+            var stat = dolgozok.ToLookup(x=>x.Reszleg).OrderByDescending(x=>x.Key);
+
+            foreach (var i in stat)
+            {
+                Console.WriteLine($"{i.Key},{i.Count()} fő, átlagbér:{i.Average(x=>x.Ber):0.0} Ft");
+            }
+
+            var stat2 = dolgozok.ToLookup(x=>new {x.Reszleg,x.Belepes}).OrderBy(x=>x.Key.Reszleg).ThenBy(x=>x.Key.Belepes);
+
+            foreach (var i in stat2)
+            {
+                Console.WriteLine($"{i.Key.Reszleg},{i.Key.Belepes},{i.Count()}");
+            }
+
+            var belepes2000Utan = dolgozok.FindAll(x => x.Belepes >= 2000);
+
+            try
+            {
+                FileStream fajl = new FileStream("belepok2000.txt", FileMode.Create);
+                StreamWriter writer = new StreamWriter(fajl,Encoding.UTF8);
+                writer.WriteLine("Név;Neme;Részleg;Belépés;Bér");
+
+                foreach (var i in belepes2000Utan)
+                {
+                    writer.WriteLine($"{i.Nev};{i.Neme};{i.Reszleg};{i.Belepes};{i.Ber}");
+                }
+
+                Console.WriteLine("Írás kész");
+                writer.Close();
+
+            }
+            catch (Exception ex)
+            {
+               Console.WriteLine(ex.Message);
+            }
+
             Console.ReadKey();
+        }
+
+        private static void Lista(List<Dolgozo> adatok)
+        {
+            foreach (var i in adatok)
+            {
+                Console.WriteLine($"{i.Nev},{i.Neme},{i.Reszleg},{i.Belepes},{i.Ber}");
+            }
         }
     }
 }
