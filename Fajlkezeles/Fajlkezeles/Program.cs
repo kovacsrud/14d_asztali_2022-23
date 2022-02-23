20630c2f793dc4bd7e90264f2809db06dcf0fb9b
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-02-23 08:23:53 +0100

    Fajlkezeles 1

---
+++ Fajlkezeles/Fajlkezeles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fajlkezeles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream fajl = new FileStream("berek2020.txt", FileMode.Open);
+                StreamReader reader = new StreamReader(fajl, Encoding.UTF8);
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
