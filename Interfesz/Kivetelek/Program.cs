52f3e1c6739f28fbe3f6cb97e55537d70520ae1d
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-03-09 10:48:27 +0100

    Saját kivétel

---
+++ Interfesz/Kivetelek/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetelek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Valami szöveg");
+                try
+                {
+                    int a = 10;
+                    int b = 5;
+                    int c = a/b;
+                    List<int> lista = null;
+                    Console.WriteLine($"C:{lista.First()}");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);                    
+                }
+
+                throw new Sajatkivetel("Saját kivétel!");
+
+                Console.WriteLine("Valami másik szöveg");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ez egy Argumentexception!");
+            }
+            catch (OutOfMemoryException ex)
+            {
+                Console.WriteLine("Out of memory!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interfesz/Kivetelek/Sajatkivetel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetelek
+{
+    public class Sajatkivetel:Exception
+    {
+        public Sajatkivetel(string uzenet):base(uzenet)
+        {
+
+        }
+    }
+}
