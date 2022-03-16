fa880f09333a5b6825401aa191e3e7a2fe3309f3
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-03-16 13:49:34 +0100

    Kutyak start

---
+++ Kutyak/Kutyak/ListaKutyanev.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutyak
+{
+    public class ListaKutyanev
+    {
+        public List<Kutyanev> Kutyanevek { get; set; }
+
+        public ListaKutyanev(string fajl,char hatarolo,int start=1)
+        {
+            Kutyanevek = new List<Kutyanev>();
+            var sorok = File.ReadAllLines(fajl, Encoding.Default);
+            for (int i = start; i <sorok.Length ; i++)
+            {
+                Kutyanevek.Add(new Kutyanev(sorok[i], hatarolo));
+            }
+        }
+    }
+}
