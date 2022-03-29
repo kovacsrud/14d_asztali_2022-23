9561ddfa9cc4a70fd449bd09823efd8fb88080a4
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-03-29 08:51:47 +0200

    Elemek

---
+++ Elemek/Elemek/Elem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elemek
+{
+    public class Elem
+    {
+        public string Ev { get; set; }
+        public string Nev { get; set; }
+        public string Vegyjel { get; set; }
+        public int Rendszam { get; set; }
+        public string Felfedezo { get; set; }
+
+        public Elem(string sor,char hatarolo)
+        {
+            var e = sor.Split(hatarolo);
+            Ev = e[0];
+            Nev = e[1];
+            Vegyjel = e[2];
+            Rendszam = Convert.ToInt32(e[3]);
+            Felfedezo = e[4];
+        }
+    }
+}
