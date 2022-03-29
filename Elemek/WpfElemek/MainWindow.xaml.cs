9e3f2c8f97c17cba34e0c465cf536de0eb3ff7c8
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-03-29 11:15:10 +0200

    Wpf

---
+++ Elemek/WpfElemek/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Elemek;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfElemek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ElemLista elemek;
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                elemek = new ElemLista("felfedezesek.csv", ';');
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+            datagridElemek.ItemsSource = elemek.Elemek;
+            
+        }
+
+        private void buttonKeres_Click(object sender, RoutedEventArgs e)
+        {
+            var vegyjel = elemek.Elemek.FindAll(x => x.Vegyjel.ToLower() == textboxVegyjel.Text.ToLower());
+            if (vegyjel.Count > 0)
+            {
+                datagridElemek.ItemsSource = vegyjel;
+            } else
+            {
+                MessageBox.Show("Nincs ilyen vegyjel!");
+            }
+        }
+
+        private void buttonReset_Click(object sender, RoutedEventArgs e)
+        {
+            datagridElemek.ItemsSource = elemek.Elemek;
+        }
+    }
+}
