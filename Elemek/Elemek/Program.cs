79df59e75ba6ed6c624133a1b1c8999530911420
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-03-29 10:15:52 +0200

    Stat

--- Elemek/Elemek/Program.cs
+++ Elemek/Elemek/Program.cs
@@ -27,9 +27,10 @@ namespace Elemek
             Console.Write("Adjon meg egy vegyjelet!");
             var vegyjel = Console.ReadLine();
 
-            
+            var mindbetu = vegyjel.All(Char.IsLetter);
 
-            while (vegyjel.Length < 1 || vegyjel.Length > 2 || !Mindbetu(vegyjel))
+            //while (vegyjel.Length < 1 || vegyjel.Length > 2 || !Mindbetu(vegyjel))
+            while (vegyjel.Length < 1 || vegyjel.Length > 2 || !vegyjel.All(Char.IsLetter))
             {
                 Console.Write("Rossz, újra!");
                 vegyjel = Console.ReadLine();
@@ -45,6 +46,28 @@ namespace Elemek
                 Console.WriteLine("Nincs ilyen elem!");
             }
 
+            var nemOkor = elemek.Elemek.FindAll(x=>x.Ev!="Ókor");
+
+            var maxKulonbseg = 0;
+
+            for (int i = 0; i < nemOkor.Count-1; i++)
+            {
+                var kulonbseg = Math.Abs(Convert.ToInt32(nemOkor[i].Ev) - Convert.ToInt32(nemOkor[i+1].Ev));
+                if (kulonbseg > maxKulonbseg)
+                {
+                    maxKulonbseg = kulonbseg;
+                }
+            }
+
+            Console.WriteLine($"A legtöbb eltelt idő:{maxKulonbseg} év.");
+
+
+            var stat = nemOkor.ToLookup(x => x.Ev).Where(x=>x.Count()>3);
+
+            foreach (var i in stat)
+            {
+                Console.WriteLine($"{i.Key}:{i.Count()} db");  
+            }
 
             Console.ReadKey();
         }
