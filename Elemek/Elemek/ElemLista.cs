8c525f4c03760e7559448ed208991f1ed17911a4
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-03-29 09:37:38 +0200

    Bekérés

---
+++ Elemek/Elemek/ElemLista.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elemek
+{
+    public class ElemLista
+    {
+        public List<Elem> Elemek { get; set; }
+
+        public ElemLista(string fajl,char hatarolo,int start=1)
+        {
+            Elemek = new List<Elem>();
+            var sorok = File.ReadAllLines(fajl, Encoding.Default);
+            for (int i = start; i < sorok.Length; i++)
+            {
+                Elemek.Add(new Elem(sorok[i], hatarolo));
+            }
+        }
+    }
+}
--- Elemek/Elemek/Program.cs
+++ Elemek/Elemek/Program.cs
@@ -10,8 +10,59 @@ namespace Elemek
     {
         static void Main(string[] args)
         {
+            ElemLista elemek = null;
+            try
+            {
+                elemek = new ElemLista("felfedezesek.csv", ';');
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine($"Elemek száma:{elemek.Elemek.Count}");
+            var okoriElemek = elemek.Elemek.FindAll(x => x.Ev == "Ókor").Count;
+            Console.WriteLine($"Ókorban felfedezett elemek:{okoriElemek}");
+
+            Console.Write("Adjon meg egy vegyjelet!");
+            var vegyjel = Console.ReadLine();
+
+            
+
+            while (vegyjel.Length < 1 || vegyjel.Length > 2 || !Mindbetu(vegyjel))
+            {
+                Console.Write("Rossz, újra!");
+                vegyjel = Console.ReadLine();
+            }
+
+            var keresettElem = elemek.Elemek.Find(x => x.Vegyjel.ToLower() == vegyjel.ToLower());
+
+            if(keresettElem!=null)
+            {
+                Console.WriteLine($"{keresettElem.Nev}");
+            } else
+            {
+                Console.WriteLine("Nincs ilyen elem!");
+            }
+
 
             Console.ReadKey();
+        }
+
+        private static bool Mindbetu(string vegyjel)
+        {
+            bool mindbetu = true;
+
+            for (int i = 0; i < vegyjel.Length; i++)
+            {
+                if (!Char.IsLetter(vegyjel[i]))
+                {
+                    mindbetu = false;
+                    break;
+                }
+            }
+
+            return mindbetu;
         }
     }
 }
