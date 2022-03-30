6eaf743958afeb00a2a86dd0d6c823a8d0928ad6
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-03-30 12:01:07 +0200

    Vgsales

---
+++ WpfVGsales/WpfVGsales/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfVGsales
+{
+    public class Game
+    {
+        public int Rank { get; set; }
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public double EuSales { get; set; }
+
+        public Game(string sor, char hatarolo)
+        {
+            var e = sor.Split(hatarolo);
+            Rank = Convert.ToInt32(e[0]);
+            Name= e[1];
+            Platform = e[2];
+            Year= Convert.ToInt32(e[3]);
+            Genre = e[4];
+            Publisher = e[5];
+            EuSales = Convert.ToDouble(e[7]);
+        }
+    }
+}
---
+++ WpfVGsales/WpfVGsales/Games.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfVGsales
+{
+    public class Games
+    {
+        public List<Game> GameSales { get; set; }
+
+        public Games(string file,char hatarolo,int start=1)
+        {
+            GameSales = new List<Game>();
+            var sorok = File.ReadAllLines(file, Encoding.Default);
+            for (int i = start; i < sorok.Length; i++)
+            {
+                GameSales.Add(new Game(sorok[i], hatarolo));
+            }
+        }
+    }
+}
