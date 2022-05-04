9ab7a62ce82f5339c97f5678bb3dbc84f31c6d39
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-05-04 10:16:19 +0200

    WebClient használata

---
+++ Wpfinternet/Wpfinternet/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpfinternet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            //https://taszi.hu/kepek/kepkezelo/large/5995.jpg
+            new WebClient().DownloadFile("https://taszi.hu/kepek/kepkezelo/large/5995.jpg", "erettsegi.jpg");
+            byte[] kepAdat=new WebClient().DownloadData("https://taszi.hu/kepek/kepkezelo/large/5995.jpg");
+            //BitmapImage bitmap = new BitmapImage(new Uri("https://taszi.hu/kepek/kepkezelo/large/5995.jpg"));
+            BitmapImage bitmap = new BitmapImage();
+            MemoryStream ms = new MemoryStream(kepAdat);
+            bitmap.BeginInit();
+            bitmap.StreamSource = ms;
+            bitmap.EndInit();
+
+            imageKep.Source = bitmap;
+
+
+
+        }
+    }
+}
