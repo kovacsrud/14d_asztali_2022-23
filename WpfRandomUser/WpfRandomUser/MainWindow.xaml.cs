6006526f6bd44f743dcbb79d76aa4e2d5ed2adcd
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-05-11 12:56:07 +0200

    Api hívás nyomtatással

---
+++ WpfRandomUser/WpfRandomUser/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfRandomUser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Users users;
+        public MainWindow()
+        {
+            InitializeComponent();
+            comboUserNumber.Items.Add(5);
+            comboUserNumber.Items.Add(10);
+            comboUserNumber.Items.Add(20);
+            comboUserNumber.Items.Add(50);
+            comboUserNumber.SelectedIndex = 0;
+        }
+
+        public Users GetUsers(int db)
+        {
+            JObject userJson = JObject.Parse(new WebClient().DownloadString($"https://randomuser.me/api/?results={db}"));
+            Users result = userJson.ToObject<Users>();
+            return result;
+        }
+
+        private void buttonLetolt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                users = GetUsers((int)comboUserNumber.SelectedItem);
+                datagridUsers.DataContext = users;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+        }
+
+        private void buttonNyomtat_Click(object sender, RoutedEventArgs e)
+        {
+            if (users != null)
+            {
+                PrintView printView = new PrintView(users);
+                printView.ShowDialog();
+            } else
+            {
+                MessageBox.Show("Először kérjen le adatokat!");
+            }
+            
+        }
+    }
+}
---
+++ WpfRandomUser/WpfRandomUser/PrintView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfRandomUser
+{
+    /// <summary>
+    /// Interaction logic for PrintView.xaml
+    /// </summary>
+    public partial class PrintView : Window
+    {
+        Users users;
+        public PrintView(Users users)
+        {
+            InitializeComponent();
+            this.users = users;
+            CreateDoc();
+        }
+
+        private void CreateDoc()
+        {
+            Paragraph cim = new Paragraph(new Run("Felhasználók listája"));
+            cim.TextAlignment = TextAlignment.Center;
+            cim.Foreground = Brushes.Blue;
+            flowUsers.Blocks.Add(cim);
+
+            foreach (var i in users.results)
+            {
+                Paragraph nev = new Paragraph();
+                nev.Inlines.Add(new Bold(new Run("Név:")));
+                nev.Inlines.Add(new Run($"{i.name.title}.{i.name.first} {i.name.last}"));
+                flowUsers.Blocks.Add(nev);
+                Paragraph telefon = new Paragraph();
+                telefon.Inlines.Add(new Bold(new Run("Telefonszámok:")));
+                telefon.Inlines.Add(new Run($"{i.phone} {i.cell}"));
+                flowUsers.Blocks.Add(telefon);
+                Paragraph kep = new Paragraph();
+                kep.Inlines.Add(new Bold(new Run("Kép:")));
+                Image userkep = new Image();
+                userkep.Source = new BitmapImage(new Uri(i.picture.large));
+                userkep.Width = 300;
+                kep.Inlines.Add(userkep);
+                flowUsers.Blocks.Add(kep);
+
+            }
+
+            
+        }
+
+        private void buttonNyomtat_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            IDocumentPaginatorSource ps = flowUsers;
+            flowUsers.PageWidth = printDialog.PrintableAreaWidth;
+            flowUsers.PageHeight = printDialog.PrintableAreaHeight;
+            flowUsers.ColumnWidth = printDialog.PrintableAreaWidth;
+            if (printDialog.ShowDialog()==true)
+            {
+                printDialog.PrintDocument(ps.DocumentPaginator, "UserPrint");
+            }
+        }
+    }
+}
