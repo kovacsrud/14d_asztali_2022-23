6b147849c1eda4d80783d004f9f506b8913268f4
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-05-18 10:47:43 +0200

    Hash

---
+++ Hash/Hash/HashMaker.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Hash
+{
+    public enum HashType
+    {
+        MD5,
+        SHA1,
+        SHA256,
+        SHA384,
+        SHA512
+    }
+
+    public class HashMaker
+    {
+        public string CreateHash(HashType type,string szoveg,bool up=false)
+        {
+            string hash = "";
+
+            if (type==HashType.MD5)
+            {
+                byte[] data;
+                MD5CryptoServiceProvider hashdata = new MD5CryptoServiceProvider();
+                if (File.Exists(szoveg))
+                {
+                    data = File.ReadAllBytes(szoveg);
+
+                } else
+                {
+                    data = new UTF8Encoding().GetBytes(szoveg);
+                }
+                hash = ByteToHash(hashdata.ComputeHash(data));
+
+            } else if (type==HashType.SHA1)
+            {
+                byte[] data;
+                SHA1CryptoServiceProvider hashdata = new SHA1CryptoServiceProvider();
+                if (File.Exists(szoveg))
+                {
+                    data = File.ReadAllBytes(szoveg);
+
+                }
+                else
+                {
+                    data = new UTF8Encoding().GetBytes(szoveg);
+                }
+                hash = ByteToHash(hashdata.ComputeHash(data));
+
+            } else if (type == HashType.SHA256)
+            {
+                byte[] data;
+                SHA256CryptoServiceProvider hashdata = new SHA256CryptoServiceProvider();
+                if (File.Exists(szoveg))
+                {
+                    data = File.ReadAllBytes(szoveg);
+
+                }
+                else
+                {
+                    data = new UTF8Encoding().GetBytes(szoveg);
+                }
+                hash = ByteToHash(hashdata.ComputeHash(data));
+            } else if (type == HashType.SHA384)
+            {
+                byte[] data;
+                SHA384CryptoServiceProvider hashdata = new SHA384CryptoServiceProvider();
+                if (File.Exists(szoveg))
+                {
+                    data = File.ReadAllBytes(szoveg);
+
+                }
+                else
+                {
+                    data = new UTF8Encoding().GetBytes(szoveg);
+                }
+                hash = ByteToHash(hashdata.ComputeHash(data));
+            } else
+            {
+                byte[] data;
+                SHA512CryptoServiceProvider hashdata = new SHA512CryptoServiceProvider();
+                if (File.Exists(szoveg))
+                {
+                    data = File.ReadAllBytes(szoveg);
+
+                }
+                else
+                {
+                    data = new UTF8Encoding().GetBytes(szoveg);
+                }
+                hash = ByteToHash(hashdata.ComputeHash(data));
+            }
+
+            if (up)
+            {
+                hash = hash.ToUpper();
+            }
+
+            return hash;
+        }
+
+        private String ByteToHash(byte[] hash)
+        {
+            StringBuilder hashString = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                hashString.Append(hash[i].ToString("x2"));
+            }
+
+            return hashString.ToString();
+        }
+    }
+}
---
+++ Hash/Hash/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string szoveg = "Hello";
+            byte[] data = new UTF8Encoding().GetBytes(szoveg);
+
+            MD5CryptoServiceProvider hash_md5 = new MD5CryptoServiceProvider();
+            SHA1CryptoServiceProvider hash_sha1 = new SHA1CryptoServiceProvider();
+            SHA256CryptoServiceProvider hash_sha256 = new SHA256CryptoServiceProvider();
+            SHA384CryptoServiceProvider hash_sha384 = new SHA384CryptoServiceProvider();
+            SHA512CryptoServiceProvider hash_sha512 = new SHA512CryptoServiceProvider();
+
+            byte[] hash = hash_md5.ComputeHash(data);
+            byte[] hashsha1 = hash_sha1.ComputeHash(data);
+            byte[] hashsha256 = hash_sha256.ComputeHash(data);
+            byte[] hashsha384 = hash_sha384.ComputeHash(data);
+            byte[] hashsha512 = hash_sha512.ComputeHash(data);
+
+            //StringBuilder hashString = ByteToHash(hash);
+
+            //Console.WriteLine(ByteToHash(hash));
+            //Console.WriteLine(ByteToHash(hashsha1));
+            //Console.WriteLine(ByteToHash(hashsha256));
+            //Console.WriteLine(ByteToHash(hashsha384));
+            //Console.WriteLine(ByteToHash(hashsha512));
+            HashMaker hashMaker = new HashMaker();
+
+            var hellohash = hashMaker.CreateHash(HashType.MD5, szoveg);
+
+            var filehash = hashMaker.CreateHash(HashType.SHA1,"nevekGUI.txt");
+
+            Console.WriteLine(hellohash);
+            Console.WriteLine(filehash);
+
+            Console.ReadKey();
+        }
+
+        private static StringBuilder ByteToHash(byte[] hash)
+        {
+            StringBuilder hashString = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                hashString.Append(hash[i].ToString("x2"));
+            }
+
+            return hashString;
+        }
+    }
+}
