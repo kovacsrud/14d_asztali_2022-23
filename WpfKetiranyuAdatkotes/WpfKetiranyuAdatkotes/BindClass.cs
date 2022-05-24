03cc91404ae5c5ce0e4ba01ed275cc7e31db5a9a
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-05-24 09:03:49 +0200

    Kétirányú adatkötés

---
+++ WpfKetiranyuAdatkotes/WpfKetiranyuAdatkotes/BindClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfKetiranyuAdatkotes
+{
+    public class BindClass:INotifyPropertyChanged
+    {
+        private int bindadat;
+        private int min, max;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int BindAdat
+        {
+            get { return bindadat; }
+
+            set
+            {
+                if (value!=bindadat && value>=min && value<=max)
+                {
+                    bindadat = value;
+                    Prop_Changed("BindAdat");
+                }
+            }
+        }
+
+        public BindClass(int ertek,int min,int max)
+        {
+            BindAdat = ertek;
+            this.min = min;
+            this.max = max;
+        }
+
+        private void Prop_Changed(string prop)
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ WpfKetiranyuAdatkotes/WpfKetiranyuAdatkotes/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKetiranyuAdatkotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindClass bindclass;
+        int min, max;
+        public MainWindow()
+        {
+            InitializeComponent();
+            min = 0;
+            max = 1200;
+            slider.Minimum = min;
+            slider.Maximum = max;
+            progressbar.Minimum = min;
+            progressbar.Maximum = max;
+            bindclass = new BindClass(50,min,max);
+            DataContext = bindclass;
+            
+
+        }
+    }
+}
