e80bbb559160e97735e39a3168b80461995574f1
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-09-13 13:20:10 +0200

    Rockets feladat

---
+++ Rockets/Rockets/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Rocket> rockets = new List<Rocket>();
+            try
+            {
+                var sorok = File.ReadAllLines("all-rockets-from-1957_v2.csv",Encoding.Default);
+
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    rockets.Add(new Rocket(sorok[i], ';'));
+                }
+                Console.WriteLine($"Betöltve, sorok száma:{rockets.Count}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);                
+            }
+
+            var raketaAr = rockets.FindAll(x=>x.Price!=-1);
+
+            //Árral rendelkező rakéták darabszáma, áruk átlaga
+            Console.WriteLine();
+
+            //foreach (var i in raketaAr)
+            //{
+            //    Console.WriteLine($"{i.Price} million");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rockets/Rockets/Rocket.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rockets
+{
+    public class Rocket
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Wiki { get; set; }
+        public string Status { get; set; }
+        //Hiányos
+        public double LiftoffThrust { get; set; }
+        public double PayloadLEO { get; set; }
+        public int Stages { get; set; }
+        //Hiányos
+        public int StrapOns { get; set; }
+        //Hiányos, m
+        public double RocketHeight { get; set; }
+        //Hiányos $ million
+        public double Price { get; set; }
+        public double PayloadGTO { get; set; }
+        //Hiányos m
+        public double FairingDiameter { get; set; }
+        //Hiányos m
+        public double FairingHeight { get; set; }
+
+        public Rocket(string sor,char hatarolo)
+        {
+            var adat = sor.Split(hatarolo);
+            Name = adat[1];
+            Company = adat[2];
+            Wiki = adat[3];
+            Status = adat[4];
+            if (adat[5].Length == 0)
+            {
+                LiftoffThrust = -1;
+            }
+            else
+            {
+                //LiftoffThrust = Convert.ToDouble(adat[5].Replace(',', '.'));
+                LiftoffThrust = Convert.ToDouble(adat[5]);
+            }
+            PayloadLEO = Convert.ToDouble(adat[6].Replace('.',','));
+
+            if (adat[7].Length==0)
+            {
+                Stages = -1;
+            } else
+            {
+                Stages = Convert.ToInt32(adat[7]);
+            }
+
+            
+
+            if (adat[8].Length==0)
+            {
+                StrapOns = -1;
+            } else
+            {
+                StrapOns = Convert.ToInt32(adat[8]);
+            }
+
+            if (adat[9].Length==0)
+            {
+                RocketHeight = -1;
+
+            } else
+            {
+                char[] off = { ' ', 'm' };
+                RocketHeight = Convert.ToDouble(adat[9].Trim(off).Replace('.', ','));
+            }
+
+            if (adat[10].Length == 0)
+            {
+                Price = -1;
+
+            }
+            else
+            {
+                char[] off = { ' ', 'm','$','i','l','o','n' };
+                Price = Convert.ToDouble(adat[10].Trim(off).Replace(",","").Replace('.', ','));
+            }
+
+            PayloadGTO = Convert.ToDouble(adat[11].Replace('.', ','));
+
+            if (adat[11].Length==0)
+            {
+                FairingDiameter = -1;
+            } else
+            {
+                char[] off = { ' ', 'm' };
+                FairingDiameter = Convert.ToDouble(adat[11].Trim(off).Replace('.', ','));
+            }
+
+            if (adat[12].Length == 0)
+            {
+                FairingHeight = -1;
+            }
+            else
+            {
+                char[] off = { ' ', 'm' };
+                FairingHeight = Convert.ToDouble(adat[12].Trim(off).Replace('.', ','));
+            }
+
+
+        }
+
+
+    }
+}
