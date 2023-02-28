01c869743f0617ff6cf0f51717f1274f5d4f1cff
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-02-28 13:21:41 +0100

    Adatkötések

---
+++ WpfMagyarVarosok/WpfMagyarVarosok/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfMagyarVarosok.Models;
+
+namespace WpfMagyarVarosok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        magyar_telepulesekContext telepulesekContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+            telepulesekContext = new magyar_telepulesekContext();
+            telepulesekContext.Telepulesek.Load();
+            telepulesekContext.Jogallas.Load();
+            telepulesekContext.Megyek.Load();
+            datagridJogallas.ItemsSource = telepulesekContext.Jogallas.Local.ToObservableCollection();
+            datagridMegyek.ItemsSource = telepulesekContext.Megyek.Local.ToObservableCollection();
+            datagridTelepulesek.ItemsSource = telepulesekContext.Telepulesek.Local.ToObservableCollection();
+
+            
+
+            DataContext = telepulesekContext;
+        }
+    }
+}
---
+++ WpfMagyarVarosok/WpfMagyarVarosok/Models/Jogallas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WpfMagyarVarosok.Models
+{
+    public partial class Jogallas
+    {
+        public Jogallas()
+        {
+            Telepulesek = new HashSet<Telepulesek>();
+        }
+
+        public int Suly { get; set; }
+        public string Jogallas1 { get; set; }
+
+        public virtual ICollection<Telepulesek> Telepulesek { get; set; }
+    }
+}
